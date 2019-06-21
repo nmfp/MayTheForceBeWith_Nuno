6d5b188f1e0c3c2f0a9df58d32429db314785bb6
Nuno Pereira <nmfpereira@gmail.com>
2019-06-21 12:21:58 +0100

    Initial Commit

---
+++ MayTheForceBeWith_Nuno/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MayTheForceBeWith_Nuno
+//
+//  Created by Nuno Pereira on 21/06/2019.
+//  Copyright © 2019 nMfpCoding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MayTheForceBeWith_NunoTests/MayTheForceBeWith_NunoTests.swift
@@ -0,0 +1,34 @@
+//
+//  MayTheForceBeWith_NunoTests.swift
+//  MayTheForceBeWith_NunoTests
+//
+//  Created by Nuno Pereira on 21/06/2019.
+//  Copyright © 2019 nMfpCoding. All rights reserved.
+//
+
+import XCTest
+@testable import MayTheForceBeWith_Nuno
+
+class MayTheForceBeWith_NunoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
