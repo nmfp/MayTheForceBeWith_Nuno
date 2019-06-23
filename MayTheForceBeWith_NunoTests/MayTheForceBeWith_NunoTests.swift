b31bc50c48e07b426a92e7fb04d7d2d9f770020a
Nuno Pereira <nmfpereira@gmail.com>
2019-06-23 04:01:52 +0100

    Unit Tests

--- MayTheForceBeWith_NunoTests/MayTheForceBeWith_NunoTests.swift
+++ MayTheForceBeWith_NunoTests/MayTheForceBeWith_NunoTests.swift
@@ -12,23 +12,30 @@ import XCTest
 class MayTheForceBeWith_NunoTests: XCTestCase {
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testCreatingUrlGetRequest() {
+        let page: Int = 2
+        let request = PersonRouter.page(page)
+        
+        XCTAssertEqual(request.url?.scheme, "https")
+        XCTAssertEqual(request.url?.host, "swapi.co")
+        XCTAssertEqual(request.url?.query, "page=\(page)")
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testParseFavouriteResponse() throws {
+        let jsonString = "{\"success\":true}"
+        let jsonData = jsonString.data(using: .utf8)
+        
+        let response = try JSONDecoder().decode(FavouriteResponse.self, from: jsonData!)
+        
+        XCTAssertEqual(response.success, true)
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testSettingAsFavourite() {
+        var personToBeFavourite = Person(name: "R2")
+        personToBeFavourite.setAsFavourite()
+        XCTAssertEqual(personToBeFavourite.isFavourite, true)
     }
-
 }
